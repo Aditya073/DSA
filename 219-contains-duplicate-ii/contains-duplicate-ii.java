927506671c382c343c2ab967151e56da09d1b334
Aditya073 <adityarajendradixit@gmail.com>
2026-07-01 10:34:55 +0530

    Time: 22 ms (90.65%) | Memory: 77 MB (65.92%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -3,36 +3,18 @@ import java.util.HashSet;
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
 
+        HashSet<Integer> hashSet = new HashSet<>(k);
+
         for (int i = 0; i < nums.length; i++) {
-            int count = 0;
-            for (int j = i+1; j < nums.length && count < k; j++){
-                count++;
-                if(nums[i] == nums[j]) return true;
+            if(hashSet.contains(nums[i])) return true;
+            hashSet.add(nums[i]);
+
+            // to maintain the window of size k
+            if(hashSet.size() > k){
+                //this removes the first element from the hashSet
+                hashSet.remove(nums[i - k]);
             }
         }
         return false;
-
-        // if (duplicate == null) return false;
-        // System.out.println(duplicate);
-        // // now we have the duplicate
-
-        // // get the index of the duplicate elements
-        // int count = 0;
-        // int[] list_of_duplicate_index = new int[nums.length];
-
-        // for (int j = 0; j < nums.length; j++) {
-        //     if (nums[j] == duplicate) {
-        //         list_of_duplicate_index[count] = j;
-        //         count++;
-        //     }
-        // }
-
-        // // check for the condition given in the question
-        // for (int z = 0; z < count - 1; z++) {
-        //     if ((list_of_duplicate_index[z + 1] - list_of_duplicate_index[z]) <= k)
-        //         return true;
-        // }
-
-        // return false;
     }
 }
