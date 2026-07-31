9867a2953da54ecfc75f37ef2b4e0004b42882b0
Aditya073 <adityarajendradixit@gmail.com>
2026-07-31 08:07:10 +0530

    Time: 19 ms (63.72%) | Memory: 47.4 MB (58.50%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        StringBuilder ans = new StringBuilder();
+        Set<Integer> removeIndex = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else if (c == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    removeIndex.add(i);
+                }
+            }
+        }
+
+        // add the remaining '(' ')' elements that were not closed 
+        while (!stack.isEmpty()) {
+            removeIndex.add(stack.pop());
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!removeIndex.contains(i)) {
+                char c = s.charAt(i);
+                ans.append(c);
+            }
+        }
+
+        return ans.toString();
+    }
+}
