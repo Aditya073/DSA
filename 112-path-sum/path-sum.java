8ad0f4421288a2fcfeffe38b863152f005a3dfef
Aditya073 <adityarajendradixit@gmail.com>
2026-08-06 19:34:57 +0530

    Time: 1 ms (4.60%) | Memory: 44.9 MB (68.55%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -15,10 +15,34 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        
+        if(root == null) return false;
 
-        if (root == null)
-            return false;
-        // Create 2 stacks for the path and the sums
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while(!stack.isEmpty()){
+            TreeNode temp = stack.pop();
+
+            if(temp.val == targetSum && temp.right == null && temp.left == null){
+                return true;
+            }
+
+            if(temp.right != null){
+                temp.right.val += temp.val;
+                stack.push(temp.right);
+            }
+            if(temp.left != null){
+                temp.left.val += temp.val;
+                stack.push(temp.left);
+            }
+        }
+
+        return false;
+    }
+}
+/*  ***************************** if modifing the tree is not allowed *****************
+   // Create 2 stacks for the path and the sums
         Stack<TreeNode> path = new Stack<>();
         Stack<Integer> sumPath = new Stack<>();
 
@@ -46,5 +70,4 @@ class Solution {
         }
 
         return false;
-    }
-}
+*/
