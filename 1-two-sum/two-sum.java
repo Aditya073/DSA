1b8c2a662610f5a981c88f875ff9ccd11af8392e
Aditya073 <adityarajendradixit@gmail.com>
2026-08-15 18:41:21 +0530

    Time: 2 ms (99.35%) | Memory: 47.5 MB (11.25%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
         //create a hashmap
         Map<Integer, Integer> hashMap = new HashMap<>();
 
         // itterate through the array
         for (int i = 0; i < nums.length; i++) {
-           int difference =  target - nums[i]; //store the difference 
-           // check if the deference is in the hashmap
-           if (hashMap.containsKey(difference)){
-               return new int[]{hashMap.get(difference), i};//return the index of the diff
-               
-           }
-           //add the value ---> as key & index ---> as value in hashmap
+            int difference = target - nums[i]; //store the difference 
+            // check if the deference is in the hashmap
+            if (hashMap.containsKey(difference)) {
+                return new int[] { hashMap.get(difference), i };//return the index of the diff
+            }
             hashMap.put(nums[i], i);
+            //add the value ---> as key & index ---> as value in hashmap
         }
-
         return null; // to avoid error
     }
 }
